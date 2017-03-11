2613f4ac88d8d5b323a537cd2974dd310ddf1e5d
Magno Miranda Dantas <magnomirandadantas@MacBook-Pro-de-Magno.local>
2017-03-11 18:22:29 -0300

    Site pessoal de Magno Miranda Dantas

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ var pessoa = {
   nome: 'Magno Miranda Dantas',
   cargo: 'Estudante da Unifacisa',
   foto: 'magno.jpg',
-  telefone: 'Telefone: (83) 9.9619-3200'
+
 }
 
 document.getElementById('nome-cabecalho').innerHTML = pessoa.nome;
 document.getElementById('cargo-cabecalho').innerHTML = pessoa.cargo;
 document.getElementById('foto-cabecalho').src = pessoa.foto;
-document.getElementById('telefone-cabecalho').innerHTML = pessoa.telefone;
