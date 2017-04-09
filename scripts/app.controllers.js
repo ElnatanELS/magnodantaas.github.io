163f38ba7ed421d3259fad150f8ad917249b4629
Magno Miranda Dantas <magnodantas1991@gmail.com>
2017-04-08 21:12:01 -0300

    Novas Alterações

--- scripts/app.controllers.js
+++ scripts/app.controllers.js
@@ -1,47 +1,52 @@
-app.controller('bodyController', function($scope){
-  $scope.eu = {
-    nome: 'Magno Miranda Dantas',
-    cargo: 'Graduando em Sistemas de Informação',
-    foto: 'images/magno.jpg',
-    telefone: '(83) 9.9619-3200',
-    links:[{
-      foto:'images/icon_gmail.png',
-      link:'mailto:magnodantas1991@gmail.com'
-    },
-    {
-      foto:'images/icon_git.png',
-      link:'https://github.com/magnodantaas'
-    },
-    {
-      foto:'images/icon_linkedin.png',
-      link:'https://www.linkedin.com/in/magno-miranda-dantas-173268101/'
-    },
-    {
-      foto:'images/icon_plus.png',
-      link:'https://plus.google.com/+MagnoDantasUFCG'
-    }]
+app.controller('bodyController', function($scope) {
+    $scope.eu = {
+        nome: 'Magno Miranda Dantas',
+        cargo: 'Graduando em Sistemas de Informação',
+        foto: 'images/magno.jpg',
+        telefone: '(83) 9.9619-3200',
+        links: [{
+                foto: 'images/icon_gmail.png',
+                link: 'mailto:magnodantas1991@gmail.com'
+            },
+            {
+                foto: 'images/icon_git.png',
+                link: 'https://github.com/magnodantaas'
+            },
+            {
+                foto: 'images/icon_linkedin.png',
+                link: 'https://www.linkedin.com/in/magno-miranda-dantas-173268101/'
+            },
+            {
+                foto: 'images/icon_plus.png',
+                link: 'https://plus.google.com/+MagnoDantasUFCG'
+            },
+            {
+                foto: 'images/icon-instagram.png',
+                link: 'https://www.instagram.com/magnomdantas/'
+            }
+        ]
 
-  };
+    };
 
-  $scope.ano = new Date().getFullYear();
+    $scope.ano = new Date().getFullYear();
 });
 
-app.controller('mainController', function($scope, MainService){
+app.controller('mainController', function($scope, MainService) {
 
 
-  $scope.curriculo = MainService.getCurriculo();
+    $scope.curriculo = MainService.getCurriculo();
 
-  $scope.orcamento = {};
-
-
-
-
-  $scope.solicitarOrcamento = function(){
-    MainService.orcamentos.push($scope.orcamento);
     $scope.orcamento = {};
+
+
+
+
+    $scope.solicitarOrcamento = function() {
+        MainService.orcamentos.push($scope.orcamento);
+        $scope.orcamento = {};
     }
 });
 
-app.controller('DashboardController', function($scope, MainService){
+app.controller('DashboardController', function($scope, MainService) {
     $scope.orcamentos = MainService.orcamentos;
 });
